776604f02bf77d0a580f8b3574b69dfa79ed5160
Youssef Yassine <yoyassin@e1r5p2.1337.ma>
2019-08-16 19:20:36 +0100

    kkkk

--- src/escape_god.c
+++ src/escape_god.c
@@ -6,7 +6,7 @@
 /*   By: yoyassin <yoyassin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/14 15:23:46 by aariss            #+#    #+#             */
-/*   Updated: 2019/08/15 10:56:05 by yoyassin         ###   ########.fr       */
+/*   Updated: 2019/08/16 19:19:21 by yoyassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,25 +49,6 @@ int		ft_isquote(int c)
 	return (0);
 }
 
-void	escape_out_quote(char *line, char **str, int *i)
-{
-	(*i)++;
-	if (line[*i] == '"')
-	{
-		*str = ft_joinchar(*str, 39);
-		*str = ft_joinchar(*str, line[*i]);
-		*str = ft_joinchar(*str, 39);
-	}
-	else if (line[*i] == 39)
-	{
-		*str = ft_joinchar(*str, '"');
-		*str = ft_joinchar(*str, line[*i]);
-		*str = ft_joinchar(*str, '"');
-	}
-	else
-		*str = ft_joinchar(*str, line[*i]);
-}
-
 char	*get_esc_char(char *str, char c)
 {
 	if (c == 'n')
@@ -80,43 +61,7 @@ char	*get_esc_char(char *str, char c)
 		str = ft_joinchar(str, '\v');
 	else if (c == 'f')
 		str = ft_joinchar(str, '\f');
-	// else if (c == keeper)
-	// {
-	// 	str = ft_joinchar(str, -7);
-	// 	str = ft_joinchar(str, c);
-	// 	str = ft_joinchar(str, -7);
-	// }
 	else
 		str = ft_joinchar(str, c);
 	return (str);
 }
-
-// char	*escape_focker(char *line)
-// {
-// 	int		i;
-// 	int		keeper;
-// 	char	*str;
-
-// 	str = ft_strdup("");
-// 	i = 0;
-// 	while (line[i])
-// 	{
-// 		if (line[i] == UQ_ESCAPE)
-// 			escape_out_quote(line, &str, &i);
-// 		else if (ft_isquote(line[i]))
-// 		{
-// 			keeper = line[i];
-// 			i++;
-// 			while (line[i] != keeper)
-// 			{
-// 				if (line[i] == 92)
-// 					str = escape_in_quote(str, line, &i, keeper);
-// 				i++;
-// 			}
-// 		}
-// 		else
-// 			str = ft_joinchar(str, line[i]);
-// 		i++;
-// 	}
-// 	return (str);
-// }
